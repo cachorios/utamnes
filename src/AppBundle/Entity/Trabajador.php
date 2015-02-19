ccf2c8d4b6a7d241640ca7da99349df61d2f2091
Cacho  Rios <cachorios@gmail.com>
2015-02-18 22:59:04 -0300

    del crud

--- src/AppBundle/Entity/Trabajador.php
+++ src/AppBundle/Entity/Trabajador.php
@@ -5,6 +5,12 @@ use Doctrine\ORM\Mapping AS ORM;
 
 /**
  * @ORM\Entity
+ * @ORM\Table(
+ *     uniqueConstraints={
+ *         @ORM\UniqueConstraint(name="Legajo unico", columns={"empleador_id","legajo"}),
+ *         @ORM\UniqueConstraint(name="cuitunico", columns={"empleador_id","cuil"})
+ *     }
+ * )
  */
 class Trabajador
 {
@@ -417,8 +423,5 @@ class Trabajador
         return $this->concepto;
     }
 
-    public function __toString(){
-        return $this->getNombre();
-    }
 
 }
