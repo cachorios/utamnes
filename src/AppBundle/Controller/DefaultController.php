b0d1987ede8c390f66e5f8b04caa4cfd42b8264e
Cacho  Rios <cachorios@gmail.com>
2015-06-25 17:57:05 -0300

    rep

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,22 +6,26 @@ namespace AppBundle\Controller;
 use JasperPHP\JasperPHP;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+
+
 
         $jr = new JasperPHP();
 
+        $base = $this->get('kernel')->getRootDir() . '/../web';
 
-        $jr->compile('boleta_bco.jrxml')->execute();
+        $jr->compile($base.'/boleta_bco.jrxml')->execute();
 
         $jr->process(
-            'boleta_bco.jasper',
+            $base.'/boleta_bco.jasper',
             false,
             array("pdf", "rtf"),
             array(),
