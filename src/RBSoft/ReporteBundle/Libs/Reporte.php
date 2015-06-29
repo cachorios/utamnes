afbeec6ecb0fa125305c7116e1fd1c4c7a216c22
Cacho  Rios <cachorios@gmail.com>
2015-06-29 08:18:33 -0300

    srv

--- src/RBSoft/ReporteBundle/Libs/Reporte.php
+++ src/RBSoft/ReporteBundle/Libs/Reporte.php
@@ -61,6 +61,7 @@ class ReporteMan
         foreach($parametros as $name => $valor){
             $params->put($name,$valor);
         }
+        $params->put("SUBREPORT_DIR","/srv/www/utamnes/web");
         return $params;
     }
 
@@ -110,16 +111,16 @@ class ReporteMan
         $jasperPrint = $fillManager->fillReport($reporte, $params, $conn);
         $ouputfile = $ouput;
 
-        /*
+
        $exporter= new Java("net.sf.jasperreports.engine.export.JRPdfExporter");
 
        $exporter->setParameter(java("net.sf.jasperreports.engine.JRExporterParameter")->JASPER_PRINT,$jasperPrint);
        $exporter->setParameter(java("net.sf.jasperreports.engine.JRExporterParameter")->OUTPUT_FILE_NAME,$ouputfile);
-        */
 
+        /*
         $exportManager = new JavaClass("net.sf.jasperreports.engine.JasperExportManager");
         $exportManager->exportReportToPdfFile($jasperPrint, $ouputfile);
-
+        */
     }
 
 }
